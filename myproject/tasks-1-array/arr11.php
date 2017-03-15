f03a738378b54ce84e5a80709098fa98a1802b22
OliaVM <oliamo2016@gmail.com>
2017-03-15 19:09:11 +0700

    15.03.2017-2

---
+++ myproject/tasks-1-array/arr11.php
@@ -0,0 +1,15 @@
+<?php
+$arr = array();
+for ($i = 0; $i < 10; $i++) {
+	$arr[$i] = rand(-100, 100);
+}
+echo "Массив без 0";
+var_dump($arr);
+	for ($i = 0; $i < count($arr); $i++) {
+		$arr2[] = $arr[$i]; 
+		if ($arr[$i] < 0 ) { 
+			$arr2[] = 0; 
+		} 
+	}
+echo "Массив с 0 после отрицательных  элементов";
+var_dump($arr2);
